bcb21d8c108660e738e2eb7d245808ff655cf1a0
takk2 <dudxkr94@gmail.com>
2021-11-04 09:18:36 +0900

    네번째 잔디

--- algorithm/index.js
+++ algorithm/index.js
@@ -162,3 +162,31 @@ function days(month) {
 		return 30;
 	}
 }
+
+// 022. 미니계산기 - 숫자 2개와 연산자를 입력받아 알맞게 계산하는 미니계산기 함수를 만들어주세요.
+// num1과 num2는 숫자열, operator는 문자열로 입력됩니다. 
+// operator "+", "/", "-", "*" 이외의 다른 값이 들어온다면 "올바른 입력이 아닙니다"라는 문구를 띄워주세요.
+
+function calculator(num1, num2, operator){
+	if (operator === "?") {
+		console.log("")
+
+	} else if(operator ==="?") {
+		console.log("")
+
+	}
+}
+
+function calculator(num1, num2, operator){
+	if (operator === "+") {
+		console.log(num1 + num2)
+	} else if(operator === "/") {
+		console.log(num1 / num2)
+	} else if(operator === "-") {
+		console.log(num1 - num2)	
+	} else if(operator === "*") {
+		console.log(num1 * num2)
+	} else {
+		console.log("올바른 입력이 아닙니다")
+	}
+}
