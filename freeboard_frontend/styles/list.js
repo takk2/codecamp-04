1673f6bc0bae778ed8998eede0ea48316a5be64d
takk2 <dudxkr94@gmail.com>
2021-11-09 22:08:39 +0900

    잔디

---
+++ freeboard_frontend/styles/list.js
@@ -0,0 +1,227 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  width: 100%;
+`
+
+export const Wrapper = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+`
+
+export const WrapperHeader = styled.div`
+  width: 1200px;
+  height: 339px;
+  padding-top: 80px;
+  margin: 0 auto;
+`
+
+export const WrapperHeaderTitle = styled.h1`
+  font-size: 36px;
+  font-weight: 700;
+  padding-bottom: 40px;
+  text-align: center;
+`
+
+export const WrapperHeaderBest = styled.div`
+  width: 1200px;
+  height: 257px;
+  display : flex;
+  /* background-color: yellow; */
+  /* flex-direction: row; */
+  justify-content: space-between;
+  `
+
+export const Best = styled.div`
+  width: 282px;
+  height: 257px;
+  display: flex;
+  flex-direction: column;
+  /* border: 1px solid black; */
+  border-radius: 20px;
+  box-shadow : 5px 5px 10px rgba(0,0,0,0.1);
+`
+
+export const BestPhoto = styled.div`
+  width: 282px;
+  height: 120px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+`
+
+export const BestTitle = styled.label`
+  width: 282px;
+  height: 27px;
+  font-size: 18px;
+  font-weight: 500;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 20px;
+`
+
+export const BestInfo = styled.div`
+  width: 282px;
+  padding-top: 20px;
+  padding-left: 20px;
+  display: flex;
+  justify-content: space-between;
+`
+
+export const InfoWriter = styled.div`
+  width: 20px;
+
+`
+
+export const WriterUser = styled.div`
+  width: 73px;
+  height: 24px;
+  display: flex;
+  padding-top: 20px;
+`
+
+export const UserIcon = styled.div`
+  width: 20px;
+  height: 20px;
+  background-color: gray;
+  padding-left: 2px;
+  padding-top: 2px;
+`
+
+export const UserName = styled.label`
+  width: 45px;
+  height: 24px;
+  font-size: 16px;
+  font-weight: 400;
+  text-align: center;
+  line-height: 24px;
+  padding-left: 6px;
+`
+export const WriteDate = styled.label`
+  width: 95px;
+  height: 18px;
+  padding-top: 8px;
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 18px;
+  color: #828282;
+`
+
+export const Like = styled.div`
+  width: 27px;
+  height: 50px;
+  border: 1px solid black;
+  margin-right: 20px;
+`
+
+export const LikeIcon = styled.img`
+  width: 20px;
+  height: 18px;
+  padding-top: 2px;
+  padding-left: 3.5px;
+`
+
+export const LikeCounter = styled.label`
+  width: 27px;
+  height: 24px;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+  padding-top: 6px;
+  text-align: center;
+`
+
+
+
+
+
+export const BodyTab = styled.div`
+  width : 1200px;
+  height: 52px;
+  /* padding-top: 592px; */
+  /* border-top: 1px solid #BDBDBD; */
+  /* border-bottom: 1px solid #000000; */
+  display: flex;
+  justify-content: space-between;
+`
+
+export const TabNum = styled.div`
+  width: 50px;
+  height: 52px;
+  color: 4f4f4f;
+  font-size: 16px;
+  line-height: 52px;
+  text-align: center;
+`
+
+export const TabTitle = styled.div`
+  width: 200px;
+  height: 52px;
+  color: 4f4f4f;
+  font-size: 16px;
+  line-height: 52px;
+  text-align: center;
+`
+
+export const TabWriter = styled.div`
+  width: 60px;
+  height: 52px;
+  color: 4f4f4f;
+  font-size: 16px;
+  line-height: 52px;
+  text-align: center;
+`
+
+export const TabDate = styled.div`
+  width: 100px;
+  height: 52px;
+  color: 4f4f4f;
+  font-size: 16px;
+  line-height: 52px;
+  text-align: center;
+`
+
+export const BodyTab1 = styled.div`
+  width : 1200px;
+  height: 52px;
+  border-top: 1px solid #BDBDBD;
+  /* border-bottom: 1px solid #000000; */
+  display: flex;
+  justify-content: space-between;
+`
+
+export const TabNum1 = styled.div`
+  width: 50px;
+  height: 52px;
+  color: 4f4f4f;
+  font-size: 16px;
+  line-height: 52px;
+  text-align: center;
+`
+
+export const TabTitle1 = styled.div`
+  width: 200px;
+  height: 52px;
+  color: 4f4f4f;
+  font-size: 16px;
+  line-height: 52px;
+  text-align: center;
+`
+
+export const TabWriter1 = styled.div`
+  width: 60px;
+  height: 52px;
+  color: 4f4f4f;
+  font-size: 16px;
+  line-height: 52px;
+  text-align: center;
+`
+
+export const TabDate1 = styled.div`
+  width: 100px;
+  height: 52px;
+  color: 4f4f4f;
+  font-size: 16px;
+  line-height: 52px;
+  text-align: center;
+`
