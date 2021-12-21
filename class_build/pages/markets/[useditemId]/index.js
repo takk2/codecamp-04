c6053a4b82af1322c457c8d3fade3f24233673c0
takk2 <dudxkr94@gmail.com>
2021-12-21 13:11:47 +0900

    21.12.21

---
+++ class_build/pages/markets/[useditemId]/index.js
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import { gql, request } from "graphql-request";
+
+export default function BoardPage() {
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content={props.fetchUseditem.name} />
+        {/* <meta property="og:url" content="http://takk2.site" /> */}
+        <meta property="og:image" content={props.fetchUseditem.images[0]} />
+        <meta property="og:description" content={props.fetchUseditem.remarks} />
+      </Head>
+      <div>안녕하세요! 상품페이지에요!!! </div>;
+    </>
+  );
+}
+
+const FETCH_USED_ITEM = gql`
+  query fetchUseditem($useditemId: ID!) {
+    fetchUseditem(useditemId: $useditemId) {
+      name
+      remarks
+      images
+    }
+  }
+`;
+
+export const getServerSideProps = async (context) => {
+  const request = await request(
+    "https://backend04.codebootcamp.co.kr/graphql",
+    FETCH_USED_ITEM,
+    { useditemId: context.query.useditemId }
+  );
+
+  return {
+    props: {
+      fetchUseditem: {
+        name: result.fetchUseditem.name,
+        remarks: result.fetchUseditem.remarks,
+        images: result.fetchUseditem.images,
+      },
+    },
+  };
+};
