b7f253267484e88b117f150a969c50b60cc6c5bc
takk2 <dudxkr94@gmail.com>
2021-12-21 11:40:01 +0900

    '21.12.21

--- class_build/pages/boards/index.js
+++ class_build/pages/boards/index.js
@@ -1,20 +1,21 @@
 import Head from "next/head";
 
 export default function BoardPage() {
-  return;
-  <>
-    <Head>
-      <meta property="og:title" content="나만의 사이트 게시판!!!" />
-      <meta property="og:url" content="http://takk2.site" />
-      <meta
-        property="og:image"
-        content="https://postfiles.pstatic.net/MjAyMTExMTFfODgg/MDAxNjM2NTk4MzgyMjY4.sj0OVEIElrDn7KVBUsN32jE7jTmZ_GEC8Z7bm-wooPkg.YN5lWDoVzIqnYGTa2MO8ocKh9pYLxqG-gW6dAy6rH6Yg.PNG.sing0982/3.png?type=w966"
-      />
-      <meta
-        property="og:description"
-        content="어서오세요! 영탁의 사이트에 오신것을 환영합니다. 포르쉐 타이칸 갖고싶어요.. "
-      />
-    </Head>
-    <div>안녕하세요! 게시판입니다!!! </div>;
-  </>;
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="나만의 사이트 게시판!!!" />
+        <meta property="og:url" content="http://takk2.site" />
+        <meta
+          property="og:image"
+          content="https://postfiles.pstatic.net/MjAyMTExMTFfODgg/MDAxNjM2NTk4MzgyMjY4.sj0OVEIElrDn7KVBUsN32jE7jTmZ_GEC8Z7bm-wooPkg.YN5lWDoVzIqnYGTa2MO8ocKh9pYLxqG-gW6dAy6rH6Yg.PNG.sing0982/3.png?type=w966"
+        />
+        <meta
+          property="og:description"
+          content="어서오세요! 영탁의 사이트에 오신것을 환영합니다. 포르쉐 타이칸 갖고싶어요.. "
+        />
+      </Head>
+      <div>안녕하세요! 게시판입니다!!! </div>;
+    </>
+  );
 }
